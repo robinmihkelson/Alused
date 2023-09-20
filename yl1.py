1aadecfac6975ac3b6e5bdca126c38cae3f4bef2
robinmihkelson <rmihkelson06@gmail.com>
2023-09-20 11:17:12 +0300

    first commit

---
+++ yl1.py
@@ -0,0 +1,5 @@
+kroonid = 15.6466
+EEK = float(input("Sisestage kroonid: "))
+EUR = EEK / kroonid
+EUR = round(EUR, 2)
+print(f"{EEK} EEK on {EUR} EUR.")
---
+++ yl2.py
@@ -0,0 +1,7 @@
+raadius = float(input ("Sisesta ringi raadius: "))
+pindala = 3.14159265359*raadius*raadius
+ümbermõõt = 2*3.14159265359*raadius
+pindala = round(pindala, 2)
+ümbermõõt = round(ümbermõõt, 2)
+print(f"Pindala: {pindala}")
+print(f"Ümbermõõt: {ümbermõõt}")
