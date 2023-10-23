c9b97c37906507d0a5e5fc2dcb147b99d6527405
Robin Mihkelson <145542575+robinmihkelson@users.noreply.github.com>
2023-10-23 15:03:23 +0300

    yl6.py

---
+++ yl6.py
@@ -0,0 +1,11 @@
+a = input("Sisesta a: ")
+b = input("Sisesta b: ")
+c = input("Sisesta c: ")
+if int(a)> int(b) and int(a)> int(c):
+
+    print("a", a)
+elif int(b)> int(c):
+
+    print("b", b)
+else:
+    print("c", c)
