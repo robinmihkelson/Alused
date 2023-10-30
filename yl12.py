ef18f4cec60b92c58b0f56bd053fa1a22a607b76
Robin Mihkelson <145542575+robinmihkelson@users.noreply.github.com>
2023-10-30 13:51:04 +0200

    yl12.py

---
+++ yl12.py
@@ -0,0 +1,15 @@
+fruitlist = ["apple", "kiwi", "cherry"]
+print(fruitlist[0])
+#fruitlist.append("pear")
+print(fruitlist)
+print(fruitlist[-1])
+fruitlist[1] = "black"
+print(fruitlist)
+if "apple" in fruitlist:
+    print("apple is in the list")
+
+print(len(fruitlist))
+fruitlist.pop(1)
+print(fruitlist)
+
+fruitlist.remove
